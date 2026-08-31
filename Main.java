ce96c9c4c698e0ec3b6bcbba55aab57b7a00b919
InOverMyHead81 <226899218+InOverMyHead81@users.noreply.github.com>
2026-08-31 13:52:14 +0100

    Add task input

--- Main.java
+++ Main.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+
         System.out.println("Task Manager");
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a task:");
+
+        String task = scanner.nextLine();
+
+        System.out.println("You entered: " + task);
 
 }
 
